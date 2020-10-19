ade8f07ca9cf646ef5aed5d9923c44b0c07ca201
J-cmlee <54999360+J-cmlee@users.noreply.github.com>
2020-10-19 13:45:28 -0400

    sum_pairs exercise

---
+++ sum_pairs.rb
@@ -0,0 +1,19 @@
+def sum_pairs(ints, s)
+  seen = {}
+  for i in ints do
+    return [s-i, i] if seen[s-i]
+    seen[i] = true
+  end
+  nil
+end
+
+l1= [1, 4, 8, 7, 3, 15]
+l2= [1, -2, 3, 0, -6, 1]
+l3= [20, -13, 40]
+l4= [1, 2, 3, 4, 1, 0]
+l5= [10, 5, 2, 3, 7, 5]
+l6= [4, -2, 3, 3, 4]
+l7= [0, 2, 0]
+l8= [5, 9, 13, -3]
+
+p sum_pairs(l1, 8)
